cf2498ca23b1613f6926832a21b7f0b3317540f9
Melissa Ryan <meryan569@gmail.com>
2015-11-15 16:47:43 -0800

    added carousel and images

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,14 @@
 class StaticPagesController < ApplicationController
-  def about
+	
+  def index
   end
 
-  def contact
-  end
+  
 
   def landing_page
   	@featured_product = Product.first
+  	@products = Product.limit(3)
+  	
   end
   
 end
