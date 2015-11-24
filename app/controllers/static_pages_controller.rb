47116d3cd608c91adfcef7f987c97a39a3357c3d
Melissa Ryan <meryan569@gmail.com>
2015-11-24 13:05:16 -0800

    added contact form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,8 +7,14 @@ class StaticPagesController < ApplicationController
 
   def landing_page
   	@featured_product = Product.first
-  	@products = Product.limit(3)
-  	
+  	@products = Product.limit(3)  	
+  end
+  
+  def thank you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	UserMailer.contact_form(@email, @name, @message).deliver_now
   end
   
 end
