751d2848fa7fcd7d29787885aaeb2e7e7137dd56
Melissa Ryan <meryan569@gmail.com>
2015-11-24 21:32:27 -0800

    updated app mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,11 +1,4 @@
 class ApplictionMailer < ActionMailer::Base
 	default from: "from@example.com"
 	layout 'mailer'
-
-	def contact_form(email, name, message)
-		@message = message
-		mail(:from => email,
-			:to => 'meryan569@gmail.com'
-			:subject => "A new contact form message from #{name}")
-	end
 end
